9592f1d59a5a1b7b266bc0b48ff84b5e2c660804
Quy <frwk.HoangQuy@gmail.com>
2023-06-13 01:06:16 +0700

    complete check function

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 document.querySelectorAll('div.taskbarElement').forEach(element => {
   element.addEventListener('click',(e) => {
-    document.querySelectorAll('div.taskbarElement').forEach(x =>{
-      x.querySelector('.elementContent').style.display = "none";
-    })
-    element.querySelector('.elementContent').style.display = "block" ;
-    //console.log(e.target.textContent);
+    var renderElementContent = document.querySelector('.renderElementContent');
+    renderElementContent.textContent = element.querySelector('.elementContent').textContent ;
+    renderElementContent.style.display = "block";
   })
 })
