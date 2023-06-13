6639cbcb0105a0529bce1b7367d3f066d903b0aa
Quy <frwk.HoangQuy@gmail.com>
2023-06-13 14:40:15 +0700

    Project Done

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-document.querySelectorAll('div.taskbarElement').forEach(element => {
-  element.addEventListener('click', (e) => {
-    //element.style.borderColor = "rgb(194, 53, 100)";
-    document.querySelectorAll('div.taskbarElement').forEach(element => {
-      element.classList.remove("highlightBorder");
-      element.classList.add('hoverElement');
-    })
-    element.classList.toggle("highlightBorder");
-    element.classList.remove("hoverElement");
-    var renderElementContent = document.querySelector('.renderElementContent');
-    renderElementContent.innerHTML = element.querySelector('.elementContent').innerHTML;
-    renderElementContent.style.display = "block";
-  })
-})
+fetch("http://localhost:3000/data")
+  .then(response => response.json())
+  .then(data => {
+    var taskbarContent = data.map((x, index) => {
+      return `
+        <div class="taskbarElement hoverElement">
+        <i class = '${data[index].icon}'></i>
+        <p class="elementTitle">${data[index].title}</p>
+        </div>
+      `
+    }).join("");
+    document.querySelector('.taskbarUI .taskbar').innerHTML = taskbarContent;
+    document.querySelectorAll('.taskbar .taskbarElement').forEach((element, index) => {
+      element.addEventListener("click", (e) => {
+        var elementContent = document.querySelector(".taskbarUI .content");
+        elementContent.classList.add('renderElementContent');
+        var content = `
+            <b> ${data[index].title} </b><br> 
+            ${data[index].content}
+          `
+        elementContent.innerHTML = content;
+        document.querySelector(".taskbarUI").appendChild(elementContent);
+        document.querySelectorAll('div.taskbarElement').forEach(element => {
+          element.classList.remove("highlightBorder");
+          element.classList.add('hoverElement');
+        })
+        element.classList.toggle("highlightBorder");
+        element.classList.remove("hoverElement");
+        var renderElementContent = document.querySelector('.renderElementContent');
+        //renderElementContent.innerHTML = element.querySelector('.elementContent').innerHTML;
+        renderElementContent.style.display = "block";
+      })
+    });
+  });
