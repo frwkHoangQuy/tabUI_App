4cc91381d2f476067db39e52d1d38f6534f3ccea
Quy <frwk.HoangQuy@gmail.com>
2023-06-13 16:55:48 +0700

    restruct code

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
 fetch("http://localhost:3000/data")
   .then(response => response.json())
   .then(data => {
-    var taskbarContent = data.map((x, index) => {
-      return `
-        <div class="taskbarElement hoverElement">
-        <i class = '${data[index].icon}'></i>
-        <p class="elementTitle">${data[index].title}</p>
-        </div>
-      `
-    }).join("");
-    document.querySelector('.taskbarUI .taskbar').innerHTML = taskbarContent;
+    renderTaskbarElement(data);
     document.querySelectorAll('.taskbar .taskbarElement').forEach((element, index) => {
-      element.addEventListener("click", (e) => {
-        var elementContent = document.querySelector(".taskbarUI .content");
-        elementContent.classList.add('renderElementContent');
-        var content = `
-            <b> ${data[index].title} </b><br> 
-            ${data[index].content}
-          `
-        elementContent.innerHTML = content;
-        document.querySelector(".taskbarUI").appendChild(elementContent);
+      element.addEventListener("click", () => {
+        renderTaskbarContent(data, index);
+        
+        //Taskbar element and content behavior
         document.querySelectorAll('div.taskbarElement').forEach(element => {
           element.classList.remove("highlightBorder");
           element.classList.add('hoverElement');
         })
         element.classList.toggle("highlightBorder");
         element.classList.remove("hoverElement");
-        var renderElementContent = document.querySelector('.renderElementContent');
-        //renderElementContent.innerHTML = element.querySelector('.elementContent').innerHTML;
-        renderElementContent.style.display = "block";
+        var taskbarContent = document.querySelector('.taskbarContent');
+        taskbarContent.style.display = "block";
       })
     });
   });
+
+var renderTaskbarElement = data => {
+  var taskbarContent = data.map((x, index) => {
+    return `
+        <div class="taskbarElement hoverElement">
+        <i class = '${data[index].icon}'></i>
+        <p class="elementTitle">${data[index].title}</p>
+        </div>
+      `
+  }).join("");
+  document.querySelector('.taskbarUI .taskbar').innerHTML = taskbarContent;
+}
+
+var renderTaskbarContent = (data, index) => {
+  var elementContent = document.querySelector(".taskbarUI .content");
+  elementContent.classList.add('taskbarContent');
+  var content = `
+        <b> ${data[index].title} </b><br> 
+        ${data[index].content}
+      `
+  elementContent.innerHTML = content;
+  document.querySelector(".taskbarUI").appendChild(elementContent);
+}
